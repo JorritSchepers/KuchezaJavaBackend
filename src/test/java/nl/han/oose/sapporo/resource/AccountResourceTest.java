23bea7d075922a650a89c28585ea268160d214ed
Jorrit Schepers <joriit.schepers1@gmail.com>
2019-11-18 12:00:20 +0100

    FIX ALLES

---
+++ src/test/java/nl/han/oose/sapporo/resource/AccountResourceTest.java
@@ -0,0 +1,13 @@
+package nl.han.oose.sapporo.resource;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountResourceTest {
+
+    @Test
+    public void buildTest() {
+        assertEquals(true, true);
+    }
+}
