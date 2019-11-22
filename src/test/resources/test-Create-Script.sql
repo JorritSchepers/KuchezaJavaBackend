59365c9749d53de489386818f8708227ea5519b1
SanneJelink <49753842+SanneJelink@users.noreply.github.com>
2019-11-22 13:25:02 +0100

    Update test-Create-Script.sql

--- src/test/resources/test-Create-Script.sql
+++ src/test/resources/test-Create-Script.sql
@@ -113,3 +113,27 @@ VALUES
 ('PatrickSt3r','DC00C903852BB19EB250AEBA05E534A6D211629D77D055033806B783BAE09937','Patrick@Ster.com'),
 ('Thomi','DC00C903852BB19EB250AEBA05E534A6D211629D77D055033806B783BAE09937','Geitenzijncool@hotmail.com'),
 ('TestUser','wachtwoord','oose.sapporo@gmail.com');
+
+Insert into inventory Values
+(1,2000),
+(2,2000),
+(3,2000);
+
+INSERT INTO plant (waterUsage,growingTime,profit,purchasePrice,name)
+VALUES
+(5,10,20,5,'Cabbage'),
+(10,5,20,7.5,'Tomato'),
+(2.5,50,40,10,'Banana');
+
+INSERT INTO plot (x,y,price,purchased)
+VALUES
+(1,1,10,1),
+(1,2,10,1),
+(1,3,10,1);
+
+update plot set plantID = 1 where plotID =2;
+
+INSERT INTO plotInFarm (farmID,plotID)
+VALUES (1,1),
+       (1,2),
+       (1,3)
