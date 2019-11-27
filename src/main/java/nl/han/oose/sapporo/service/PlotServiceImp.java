0c6dbc587d624df418ebfa52513d9a44e393e74a
SanneJelink <49753842+SanneJelink@users.noreply.github.com>
2019-11-27 16:10:08 +0100

    Oogsten aanpassen naar alleen afhankelijk van id

--- src/main/java/nl/han/oose/sapporo/service/PlotServiceImp.java
+++ src/main/java/nl/han/oose/sapporo/service/PlotServiceImp.java
@@ -3,6 +3,7 @@ package nl.han.oose.sapporo.service;
 import nl.han.oose.sapporo.dto.PlantDTO;
 import nl.han.oose.sapporo.dto.PlotDTO;
 import nl.han.oose.sapporo.dto.UserDTO;
+import nl.han.oose.sapporo.persistence.IPlantDAO;
 import nl.han.oose.sapporo.persistence.IPlotDAO;
 
 import javax.enterprise.inject.Default;
@@ -12,8 +13,14 @@ import java.util.ArrayList;
 @Default
 public class PlotServiceImp implements IPlotService {
     private IPlotDAO plotDAO;
+    private IPlantDAO plantDAO;
     private IInventoryService inventoryService;
     private IPlantService plantService;
+
+    @Inject
+    public void setPlantDAO(IPlantDAO plantDAO) {
+        this.plantDAO = plantDAO;
+    }
 
     @Inject
     public void setPlotDAO(IPlotDAO plotDAO) {
@@ -44,7 +51,8 @@ public class PlotServiceImp implements IPlotService {
     public PlotDTO harvesPlant(PlantDTO plantDTO, UserDTO user, int plotID) {
         if (plantService.plantFullGrown(plantDTO) && plotDAO.plotHasPlant(plotID)) {
             plotDAO.removeObjectsFromPlot(plotID);
-            inventoryService.increaseSaldo(plantDTO.getProfit(), user);
+            int price = plantDAO.getProfit(plantDTO.getID());
+            inventoryService.increaseSaldo(price, user);
             return plotDAO.getPlot(plotID);
         }
         return null;
