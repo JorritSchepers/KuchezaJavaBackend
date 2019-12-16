519784323ef5286343b0781d8f73c16ab88e723d
IndeWater <IndeWater@users.noreply.github.com>
2019-12-16 10:36:56 +0100

    niet alle dtos meer importen

--- src/main/java/nl/han/oose/sapporo/service/PlotServiceImp.java
+++ src/main/java/nl/han/oose/sapporo/service/PlotServiceImp.java
@@ -1,6 +1,11 @@
 package nl.han.oose.sapporo.service;
 
-import nl.han.oose.sapporo.dto.*;
+import nl.han.oose.sapporo.dto.PlantDTO;
+import nl.han.oose.sapporo.dto.PlotDTO;
+import nl.han.oose.sapporo.dto.UserDTO;
+import nl.han.oose.sapporo.dto.AllPlotDTO;
+import nl.han.oose.sapporo.dto.AnimalDTO;
+import nl.han.oose.sapporo.dto.FarmDTO;
 import nl.han.oose.sapporo.persistence.IFarmDAO;
 import nl.han.oose.sapporo.persistence.IPlantDAO;
 import nl.han.oose.sapporo.persistence.IPlotDAO;
