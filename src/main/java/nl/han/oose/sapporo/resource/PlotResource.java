52526cf49b58e333d053ccfceceecdafee6374ef
SanneJelink <49753842+SanneJelink@users.noreply.github.com>
2019-12-19 13:38:55 +0100

    Update PlotResource.java

--- src/main/java/nl/han/oose/sapporo/resource/PlotResource.java
+++ src/main/java/nl/han/oose/sapporo/resource/PlotResource.java
@@ -88,6 +88,7 @@ public class PlotResource {
                 .build();
     }
 
+    @POST
     @Path("/{id}/animal")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
