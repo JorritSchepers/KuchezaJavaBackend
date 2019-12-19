fd0513d9cf8e75c541dfa4893abeb3f4c5d21180
SanneJelink <49753842+SanneJelink@users.noreply.github.com>
2019-12-19 14:08:27 +0100

    Ophalen acties toegevoegd
    Acties kunnen nu via de admin met een userID worden opgehaald

---
+++ src/main/java/nl/han/oose/sapporo/dto/AllActionsDTO.java
@@ -0,0 +1,11 @@
+package nl.han.oose.sapporo.dto;
+
+import java.util.ArrayList;
+
+public class AllActionsDTO {
+    private ArrayList<ActionDTO> actions;
+
+    public AllActionsDTO(ArrayList<ActionDTO> actions) {
+        this.actions = actions;
+    }
+}
--- src/main/java/nl/han/oose/sapporo/persistence/IActionDao.java
+++ src/main/java/nl/han/oose/sapporo/persistence/IActionDao.java
@@ -1,5 +1,9 @@
 package nl.han.oose.sapporo.persistence;
 
+import nl.han.oose.sapporo.dto.AllActionsDTO;
+
 public interface IActionDao {
     void insertAction(int id, int actionID, String affectedName, int water, int money);
+
+    AllActionsDTO getUserActions(int userID);
 }
